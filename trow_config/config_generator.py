e9b4ff4c91dacaa9f62657bb5f8915698013e068
Shreyas Acharya <shreyasacharya@COMLAP3458.local>
2025-07-09 15:41:58 +0530

    Update config_generator.py : update the logic to work with environment variables

--- trow_config/config_generator.py
+++ trow_config/config_generator.py
@@ -1,9 +1,34 @@
 """Generate the trow configuration."""
 
+import os
 from pathlib import Path
 
 import boto3
 from configuration import RegistryConfig, RegistryType
+
+
+def _retrieve_value(configuration: dict) -> str:
+    """Retrieve the value from the configuration."""
+    if len(configuration) != 1:
+        error = ValueError(
+            "Invalid configuration. Exactly one value from the following"
+            " keys must be provided: value, file, env"
+        )
+        raise error
+
+    if "value" in configuration:
+        return configuration["value"]
+    if "file" in configuration:
+        with Path.open(
+            Path(configuration["file"]),
+            encoding="utf-8",
+        ) as file:
+            return file.read()
+    if "env" in configuration:
+        return os.environ[configuration["env"]]
+
+    error = ValueError("Invalid configuration. Unknown option.")
+    raise error
 
 
 def generate_trow_configuration(registries: list[RegistryConfig]) -> dict:
@@ -18,8 +43,12 @@ def generate_trow_configuration(registries: list[RegistryConfig]) -> dict:
                 {
                     "alias": registry.alias,
                     "host": registry.host,
-                    "username": registry.auth_configuration["username"],
-                    "password": registry.auth_configuration["password"],
+                    "username": _retrieve_value(
+                        registry.auth_configuration["username"]
+                    ),
+                    "password": _retrieve_value(
+                        registry.auth_configuration["password"]
+                    ),
                 },
             )
         elif registry.registry_type in [
@@ -27,21 +56,27 @@ def generate_trow_configuration(registries: list[RegistryConfig]) -> dict:
             RegistryType.ECR,
         ]:
             with Path.open(
-                Path(registry.auth_configuration["web_identity_token_file"]),
+                Path(
+                    _retrieve_value(
+                        registry.auth_configuration["web_identity_token_file"]
+                    )
+                ),
                 encoding="utf-8",
             ) as file:
                 token = file.read()
 
             credentials = boto3.client("sts").assume_role_with_web_identity(
-                RoleArn=registry.auth_configuration["role_arn"],
-                RoleSessionName=registry.auth_configuration["role_session_name"],
+                RoleArn=_retrieve_value(registry.auth_configuration["role_arn"]),
+                RoleSessionName=_retrieve_value(
+                    registry.auth_configuration["role_session_name"]
+                ),
                 WebIdentityToken=token,
             )
 
             if registry.registry_type == RegistryType.ECR_PUBLIC:
                 client = boto3.client(
                     "ecr-public",
-                    region_name=registry.auth_configuration["region"],
+                    region_name=_retrieve_value(registry.auth_configuration["region"]),
                     aws_access_key_id=credentials["Credentials"]["AccessKeyId"],
                     aws_secret_access_key=credentials["Credentials"]["SecretAccessKey"],
                     aws_session_token=credentials["Credentials"]["SessionToken"],
@@ -58,13 +93,15 @@ def generate_trow_configuration(registries: list[RegistryConfig]) -> dict:
             else:
                 client = boto3.client(
                     "ecr",
-                    region_name=registry.auth_configuration["region"],
+                    region_name=_retrieve_value(registry.auth_configuration["region"]),
                     aws_access_key_id=credentials["Credentials"]["AccessKeyId"],
                     aws_secret_access_key=credentials["Credentials"]["SecretAccessKey"],
                     aws_session_token=credentials["Credentials"]["SessionToken"],
                 )
                 response = client.get_authorization_token(
-                    registryIds=[registry.auth_configuration["registry_id"]],
+                    registryIds=[
+                        _retrieve_value(registry.auth_configuration["registry_id"])
+                    ]
                 )
                 trow_configuration["registry_proxies"]["registries"].append(
                     {
